5b9eeaf5c800e229079929d6aaf8adfb87b19019
Sarah Schaller <sarcsch@aol.com>
2017-11-07 15:38:17 +0000

    automatically backed up by learn

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,5 +1,5 @@
 class Author < ActiveRecord::Base
-  validates :name, prescence: true
+  validates :name, presence: true
   validates :email, uniqueness: true
   validates :phone_number, length: { in: 10 }
 end
